89e1ba9c277609edc62f24df6427ddc76a58c2be
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-08-04 13:53:12 +0800

    Finish training ct models

--- UNET/pyimagesearch/config.py
+++ UNET/pyimagesearch/config.py
@@ -2,7 +2,7 @@ import torch
 import os
 
 # base path of dataset
-DATASET_PATH = "../dataset_pt_empty_mask_removed"
+DATASET_PATH = "../dataset_ct_empty_mask_removed"
 
 # define the path to the training set
 IMAGE_DATASET_PATH = os.path.sep.join([DATASET_PATH, "train", "images"])
