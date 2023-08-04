b7e8748e9aa0b2b301cec2448dbcccc2ce05f898
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-08-04 12:27:47 +0800

    Restructure dataset

--- UNET/pyimagesearch/config.py
+++ UNET/pyimagesearch/config.py
@@ -2,7 +2,7 @@ import torch
 import os
 
 # base path of dataset
-DATASET_PATH = "./dataset_pt"
+DATASET_PATH = "../dataset_mr_mask_removed"
 
 # define the path to the training set
 IMAGE_DATASET_PATH = os.path.sep.join([DATASET_PATH, "train", "images"])
