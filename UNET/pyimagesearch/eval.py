6f101aef72f408f89bb57b0c8a284e4dc46b2cd9
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-08-09 21:56:01 +0800

    Change import of config

--- UNET/pyimagesearch/eval.py
+++ UNET/pyimagesearch/eval.py
@@ -1,5 +1,5 @@
 import torch
-import config
+from pyimagesearch import config
 
 def dice_score(pred, target, threshold=config.THRESHOLD, eps=1e-6):
     pred = (pred > threshold).float() # Threshold the prediction
