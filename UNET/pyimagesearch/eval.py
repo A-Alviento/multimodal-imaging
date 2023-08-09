6a0d985f8fddd0ed61c128f66c354941e7e18d5e
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-08-09 21:50:53 +0800

    Import config and replace threshold with that in config

--- UNET/pyimagesearch/eval.py
+++ UNET/pyimagesearch/eval.py
@@ -1,11 +1,12 @@
 import torch
+import config
 
 def dice_score(pred, target, eps=1e-7):
     """
     Compute the Dice coefficient given predicted and target masks.
     """
     # Convert predictions to binary using a threshold of 0.5
-    pred = torch.sigmoid(pred) >= 0.5
+    pred = torch.sigmoid(pred) >= config.THRESHOLD
     intersection = (pred & target).float().sum()
     union = pred.float().sum() + target.float().sum()
     dice = (2 * intersection) / (union + eps)
