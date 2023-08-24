3a17d2d32fdd5d27b830cfedae6e974aac0139d2
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-08-24 17:03:31 +0800

    Import bce loss function

--- UNET/pyimagesearch/eval.py
+++ UNET/pyimagesearch/eval.py
@@ -1,6 +1,7 @@
 import torch
 from pyimagesearch import config
 import torch.nn as nn
+from torch.nn import BCEWithLogitsLoss
 
 def dice_score(pred, target, threshold=config.THRESHOLD, eps=1e-6):
     pred = (pred > threshold).float() # Threshold the prediction
